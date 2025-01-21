7461a6a183b5acca9c44c8a8c50d80bf4929f119
Hansraj Kondeda <82136657+Hansraj-k@users.noreply.github.com>
2025-01-21 21:05:15 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -39,3 +39,28 @@ self.addEventListener('activate', (event) => {
         )
     );
 });
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('counter-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                'index.html',
+                'style.css',
+                'script.js',
+                'rkhkmc.jpg',
+                'rkhkmc.png',
+                'rkhkmc.mp3'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
