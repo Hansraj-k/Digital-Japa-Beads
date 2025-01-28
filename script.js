3aa3da2915b517674db1c2221a93d23bdd18128a
Hansraj Kondeda <82136657+Hansraj-k@users.noreply.github.com>
2025-01-28 22:35:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -301,15 +301,18 @@ function showImageChangeMenu(event) {
     menu.style.top = `${event.pageY}px`;
 }
 
-// Check if the popup has been shown before
-if (!localStorage.getItem('popupShown')) {
-    // Show the popup if not shown before
-    document.getElementById('popupnotify').style.display = 'flex';
+document.addEventListener('DOMContentLoaded', function () {
+    // Check if the popup has been shown before
+    if (!localStorage.getItem('popupShown')) {
+        // Show the popup if not shown before
+        const popup = document.getElementById('popupnotify');
+        popup.style.display = 'flex';
 
-    // Event listener to close the popup
-    document.getElementById('close-popup-notify').addEventListener('click', function () {
-        // Hide the popup and mark it as shown
-        document.getElementById('popupnotify').style.display = 'none';
-        localStorage.setItem('popupShown', 'true'); // Mark as shown
-    });
-}
+        // Event listener to close the popup
+        document.getElementById('close-popup-notify').addEventListener('click', function () {
+            // Hide the popup and mark it as shown
+            popup.style.display = 'none';
+            localStorage.setItem('popupShown', 'true'); // Mark as shown
+        });
+    }
+});
