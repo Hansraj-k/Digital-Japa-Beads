5b0719e70a1a536b19df2b35f8f5a1fa66c1060f
Hansraj Kondeda <82136657+Hansraj-k@users.noreply.github.com>
2025-03-29 11:28:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -193,7 +193,7 @@ function adjustCircleContainer() {
     // Base padding values
     let basePadding;
     if (isMobile) {
-        basePadding = isPWAInstalled ? 210 : 492;
+        basePadding = isPWAInstalled ? 360 : 492;
     } else {
         basePadding = window.innerWidth <= 768 ? 480 : 423;
     }
