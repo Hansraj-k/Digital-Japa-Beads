36ec20545796724b282cfac01d96fcdff036efcc
Hansraj Kondeda <82136657+Hansraj-k@users.noreply.github.com>
2025-04-03 19:54:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -216,9 +216,9 @@ function adjustCircleContainer() {
 
     let basePadding;
     if (isMobile) {
-        basePadding = isPWAInstalled ? 375 : 450;
+        basePadding = isPWAInstalled ? 375 : 480;
     } else {
-        basePadding = window.innerWidth <= 768 ? 465 : 423;
+        basePadding = window.innerWidth <= 768 ? 435 : 423;
     }
 
     const sizeAdjustment = (currentButtonSize - 100) * 3; // 3px per 10%
