8fb6e3fdd65a8061a924008082c88fa59ba08201
Hansraj Kondeda <82136657+Hansraj-k@users.noreply.github.com>
2025-07-13 12:35:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -676,7 +676,7 @@ updateRoundDisplay();
 updateCircleText();
 loadSettingsFromStorage();
 initializeStreak();
- // Initial daily check
+checkAndResetDaily(); // Initial daily check
 
 // Set up a daily check (every hour to be safe)
 setInterval(checkAndResetDaily, 60 * 60 * 1000);
